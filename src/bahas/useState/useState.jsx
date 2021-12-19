bb4ca5f53a6a04f82de1989431c187daa3236976
Naufal Adriel Fauzi <fnaufaladriel@gmail.com>
2021-12-19 12:07:51 +0700

    i hilang

--- src/bahas/useState/useState.jsx
+++ src/bahas/useState/useState.jsx
@@ -1,28 +1,27 @@
-import {useState} from "react";
-const UseState=()=>{
-    // 1. counter
-    // let [itung,setItung]=useState(0);
-    // return(
-    //     <>
-    //     <button onClick={()=>setItung(itung-1)}>-</button>
-    //     <span> {itung} </span>
-    //     <button onClick={()=>setItung(itung+1)}>+</button>
-    //     </>
-    // )
-    // 2. ganti nama
-    let i=0;
-    let jeneng=["cindil","joko","Dandi"]
-    let [nama,setNama]=useState(jeneng[0]);
-    console.log(nama)
-   
-    return(
-        <>
-            <button onClick={()=>setNama(jeneng[1])}>Ganti Nama</button>
-            <span> {nama}</span>
-        </>
-    )
-}
-export default UseState
+import { useState } from 'react';
+const UseState = () => {
+  // 1. counter
+  // let [itung,setItung]=useState(0);
+  // return(
+  //     <>
+  //     <button onClick={()=>setItung(itung-1)}>-</button>
+  //     <span> {itung} </span>
+  //     <button onClick={()=>setItung(itung+1)}>+</button>
+  //     </>
+  // )
+  // 2. ganti nama
+  let jeneng = ['cindil', 'joko', 'Dandi'];
+  let [nama, setNama] = useState(jeneng[0]);
+  console.log(nama);
+
+  return (
+    <>
+      <button onClick={() => setNama(jeneng[1])}>Ganti Nama</button>
+      <span> {nama}</span>
+    </>
+  );
+};
+export default UseState;
 // penjelasan useState:
 // jadi yg aku tangkep adalah useState itu untuk update data
 // per de-construct dulu objectnya. useState terdiri dari state dan setState
